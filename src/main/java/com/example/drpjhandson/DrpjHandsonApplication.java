1cad0d9d6d170f18846bc916d4bf21d5f96f30bf
nishioka92 <seiki09020502@yahoo.co.jp>
2022-09-16 18:14:57 +0900

    Initial commit

---
+++ src/main/java/com/example/drpjhandson/DrpjHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.drpjhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrpjHandsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrpjHandsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/drpjhandson/DrpjHandsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drpjhandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrpjHandsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
